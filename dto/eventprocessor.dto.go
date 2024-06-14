e5b84636be4c6b80235574006aa5ba7b9b210c37
KickTricks <kicktricks@outlook.com>
2024-06-14 02:00:33 +0200

    Change event data from string to interface

--- dto/eventprocessor.dto.go
+++ dto/eventprocessor.dto.go
@@ -4,5 +4,5 @@ import "github.com/RebotifyOrg/crosscutting/platform"
 
 type EventProcessorMessageDTO struct {
 	Platform platform.Platform `json:"platform"`
-	Data     []byte            `json:"data"`
+	Data     interface{}       `json:"data"`
 }
