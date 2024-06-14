8ffad5d1716e85848c90bf09293b8938f663bb55
KickTricks <kicktricks@outlook.com>
2024-06-14 02:03:43 +0200

    Refactor

--- dto/internal.dto.go
+++ dto/internal.dto.go
@@ -1,15 +1,10 @@
 package dto
 
 import (
-	"github.com/RebotifyOrg/crosscutting/internalevent"
 	"github.com/google/uuid"
 )
 
 type BlackjackInterimMessageDTO struct {
-	Event internalevent.InternalEvent    `json:"event"`
-	Data  BlackjackInterimMessageDataDTO `json:"data"`
-}
-type BlackjackInterimMessageDataDTO struct {
 	UserId  uuid.UUID `json:"userId"`
 	Enabled bool      `json:"enabled"`
 }
