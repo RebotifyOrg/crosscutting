00fb629d79953fc8dd069bfb54baf9b22a5ac0ad
KickTricks <kicktricks@outlook.com>
2024-06-21 06:04:39 +0200

    Switch from interface to rawmessage

--- dto/eventprocessor.dto.go
+++ dto/eventprocessor.dto.go
@@ -1,13 +1,15 @@
 package dto
 
 import (
+	"encoding/json"
+
 	"github.com/RebotifyOrg/crosscutting/internalevent"
 	"github.com/RebotifyOrg/crosscutting/platform"
 )
 
 type EventProcessorMessageDTO struct {
 	Platform platform.Platform `json:"platform"`
-	Data     interface{}       `json:"data"`
+	Data     json.RawMessage   `json:"data"`
 }
 
 type EventProcessorMessageDataDTO struct {
