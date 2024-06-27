e42006e0d09fc33bfc771c7349b1a1241a20285e
KickTricks <henrik.helgesen.es@gmail.com>
2024-06-27 20:08:27 +0200

    Adding marshalling to all DTOs

--- dto/gameserver.dto.go
+++ dto/gameserver.dto.go
@@ -1,6 +1,8 @@
 package dto
 
 import (
+	"encoding/json"
+
 	"github.com/RebotifyOrg/crosscutting/gametype"
 	"github.com/RebotifyOrg/crosscutting/gametypeevent"
 	"github.com/google/uuid"
@@ -11,8 +13,25 @@ type GameServerMessageDTO struct {
 	UserId   uuid.UUID                `json:"userId"`
 	Data     GameServerMessageDataDTO `json:"data"`
 }
+
+func (i GameServerMessageDTO) MarshalBinary() ([]byte, error) {
+	return json.Marshal(i)
+}
+
+func (i *GameServerMessageDTO) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, i)
+}
+
 type GameServerMessageDataDTO struct {
 	Event    gametypeevent.GameTypeEvent `json:"event"`
 	UserId   uuid.UUID                   `json:"userId"`
 	Username string                      `json:"username"`
+}
+
+func (i GameServerMessageDataDTO) MarshalBinary() ([]byte, error) {
+	return json.Marshal(i)
+}
+
+func (i *GameServerMessageDataDTO) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, i)
 }
--- dto/internal.dto.go
+++ dto/internal.dto.go
@@ -1,10 +1,20 @@
 package dto
 
 import (
+	"encoding/json"
+
 	"github.com/google/uuid"
 )
 
 type BlackjackInterimMessageDTO struct {
 	UserId  uuid.UUID `json:"userId"`
 	Enabled bool      `json:"enabled"`
+}
+
+func (i BlackjackInterimMessageDTO) MarshalBinary() ([]byte, error) {
+	return json.Marshal(i)
+}
+
+func (i *BlackjackInterimMessageDTO) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, i)
 }
